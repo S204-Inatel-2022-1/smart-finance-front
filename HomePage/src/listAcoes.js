df80769c0010f85680097482d5d438ea3401a1bd
Vitor <vitorandre26@gmail.com>
2022-06-21 02:14:04 -0300

    finalversion

--- HomePage/src/listAcoes.js
+++ HomePage/src/listAcoes.js
@@ -13,7 +13,7 @@ xhrGet.onreadystatechange = function() {
     }
 }
 
-xhrGet.open('GET', 'http://localhost:3000/comprar-acoes')
+xhrGet.open('GET', 'http://smart-finance-back.herokuapp.com/comprar-acoes')
 xhrGet.send()
 
 function listAcoes(acao){
